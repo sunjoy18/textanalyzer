d366269ed0cba0a717bb2ced1da092fce189960c
Sanjay Gupta <sanjayguptasarvjeet342@gmail.com>
2023-04-17 00:36:59 +0530

    Commit9

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,45 +3,16 @@ import React from 'react'
 export default function About(props) {
   return (
     <div className='about'>
-      <div className="accordion" id="accordionExample" >
-        <div className="accordion-item" style={{backgroundColor: props.mode==='dark'?'#656983':'white', color: props.mode==='dark'?'white':'black'}}>
-            <h2 className="accordion-header">
-            <button className="accordion-button" style={{backgroundColor: props.mode==='dark'?'#0081ff':'white', color: props.mode==='dark'?'white':'black'}} type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                Accordion Item #1
-            </button>
-            </h2>
-            <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
-            <div className="accordion-body">
-                <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classNamees that we use to style each element. These classNamees control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-            </div>
-            </div>
-        </div>
-        <div className="accordion-item" style={{backgroundColor: props.mode==='dark'?'#656983':'white', color: props.mode==='dark'?'white':'black'}}>
-            <h2 className="accordion-header">
-            <button className="accordion-button collapsed" style={{backgroundColor: props.mode==='dark'?'#0081ff':'white', color: props.mode==='dark'?'white':'black'}} type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                Accordion Item #2
-            </button>
-            </h2>
-            <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
-            <div className="accordion-body">
-                <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classNamees that we use to style each element. These classNamees control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-            </div>
-            </div>
-        </div>
-        <div className="accordion-item" style={{backgroundColor: props.mode==='dark'?'#656983':'white', color: props.mode==='dark'?'white':'black'}}>
-            <h2 className="accordion-header">
-            <button className="accordion-button collapsed" style={{backgroundColor: props.mode==='dark'?'#0081ff':'white', color: props.mode==='dark'?'white':'black'}} type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                Accordion Item #3
-            </button>
-            </h2>
-            <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
-            <div className="accordion-body">
-                <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classNamees that we use to style each element. These classNamees control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-            </div>
-            </div>
-        </div>
-        </div>
-        <br />
+      
+        <h1 my-3 style={{color: props.mode==='dark'?'white':'black'}}>About Us</h1>
+        <p>Itna khali time hai tere pass ke Word Analyzer website ka about padh raha hai
+            <br /><br /><br />
+            <p style={{color: props.mode==='dark'?'white':'black'}}> : Fir bhi Sun le agya to : </p>
+            <br />
+            TextAnalyzer is an utility which is used to manipulate texts the way you want. <br /> Such as convert texts to any case (Upper or Lower), count words and characters, get average read time, remove extra spaces, get it's preview.
+        </p>
+      
+      <br />
     </div>
   )
 }
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -9,29 +9,31 @@ function Contact() {
   return (    
     <div className='contact'> 
         <small>Enter message (optional) and click button "Send"</small>
-        <div className="wrapper centered">
-        <article className="letter">
-            <div className="side">
-            <h1>Contact us</h1>
-            <p>
-                <textarea placeholder="Your message"></textarea>
-            </p>
+        <form action="mailto:sanjayguptasarvjeet342@gmail.com" method="post" enctype="text/plain">
+            <div className="wrapper centered">
+            <article className="letter">
+                <div className="side">
+                <h1>Contact us</h1>
+                <p>
+                    <textarea placeholder="Your message" name='Message'></textarea>
+                </p>
+                </div>
+                <div className="side">
+                <p>
+                    <input type="text" placeholder="Your name" name='Name'/>
+                </p>
+                <p>
+                    <input type="tel" size="13" placeholder="Your contact" name="Contact"/>
+                </p>
+                <p>
+                    <button id="sendLetter" onClick={sentLetter}>Send</button>
+                </p>
+                </div>
+            </article>
+            <div className="envelope front"></div>
+            <div className="envelope back"></div>
             </div>
-            <div className="side">
-            <p>
-                <input type="text" placeholder="Your name" />
-            </p>
-            <p>
-                <input type="email" placeholder="Your email" />
-            </p>
-            <p>
-                <button id="sendLetter" onClick={sentLetter}>Send</button>
-            </p>
-            </div>
-        </article>
-        <div className="envelope front"></div>
-        <div className="envelope back"></div>
-        </div>
+        </form>
         <p className="result-message centered">Thank you for your message</p>
     </div>
   )
